59606e09995385d33edcabe0aa8c8bec43965eb4
Filip Tomevski <ftomevski16@gmail.com>
2022-10-27 20:34:48 +0200

    Intial commit, project is finished

---
+++ DomashnaEden/src/myProject/domashnaEden.java
@@ -0,0 +1,10 @@
+package myProject;
+
+public class domashnaEden {
+	
+	public static void main(String args[]) {
+	System.out.println("Hello World OOP FIKT");
+	// Link do GitHub repository:
+	// https://github.com/FilipTheDev/oopDomasno1
+	}
+}
